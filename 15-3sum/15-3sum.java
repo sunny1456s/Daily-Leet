eb4559bed35480c3af7c7b437a69ecdf34bfbb37
SUNNY BANSODE <sunnybansode7789@gmail.com>
2026-07-03 23:29:27 +0530

    Create 15-3sum

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,55 @@
+// Last updated: 7/3/2026, 11:29:27 PM
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        // Step 1: Sort the array
+        Arrays.sort(nums);
+        
+        // Step 2: Fix one element
+        for (int i = 0; i < nums.length - 2; i++) {
+            
+            // Skip duplicate values for i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            // Step 3: Two pointer search
+            while (left < right) {
+                
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    left++;
+                    right--;
+                    
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
