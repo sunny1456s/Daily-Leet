d5e530a23cf6fc466ca64fd2ce9edc4af6ffe704
SUNNY BANSODE <sunnybansode7789@gmail.com>
2026-07-03 23:29:02 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,29 @@
+// Last updated: 7/3/2026, 11:29:02 PM
+class MinStack {
+    private List<int[]> st;
+
+    public MinStack() {
+        st = new ArrayList<>();
+    }
+    
+    public void push(int val) {
+        int[] top = st.isEmpty() ? new int[]{val, val} : st.get(st.size() - 1);
+        int min_val = top[1];
+        if (min_val > val) {
+            min_val = val;
+        }
+        st.add(new int[]{val, min_val});        
+    }
+    
+    public void pop() {
+        st.remove(st.size() - 1);
+    }
+    
+    public int top() {
+        return st.isEmpty() ? -1 : st.get(st.size() - 1)[0];
+    }
+    
+    public int getMin() {
+        return st.isEmpty() ? -1 : st.get(st.size() - 1)[1];
+    }
+}
