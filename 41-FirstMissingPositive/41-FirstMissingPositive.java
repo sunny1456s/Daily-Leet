38a2110d59d018e684b236c6c01c8d4fb325874c
SUNNY BANSODE <sunnybansode7789@gmail.com>
2026-07-03 23:29:19 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -0,0 +1,19 @@
+// Last updated: 7/3/2026, 11:29:18 PM
+class Solution {
+
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1]; 
+        for (int num : nums) {
+            if (num > 0 && num <= n) {
+                seen[num] = true;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+}
